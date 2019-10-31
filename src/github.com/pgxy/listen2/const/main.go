b02fb8fe49ec5e7d5c2f0bf4a517017f50bd0d49
tddey01 <kevinlandun@gmail.com>
2019-10-31 11:47:37 +0800

    常量基础语法类型操作

--- src/github.com/pgxy/listen2/const/main.go
+++ src/github.com/pgxy/listen2/const/main.go
@@ -31,4 +31,18 @@ func main() {
 	// )
 
 	fmt.Printf("a = %d b = %d  c = %d d = %d \n",a,b,c,d)
+
+	const (
+		e = iota
+		f
+		g
+	)
+	fmt.Printf("e = %d  f = %d g = %d\n", e,f,g)
+
+	const (
+		a1 = 1 << iota  //
+		a2
+		a3
+	)
+	fmt.Printf("a1 = %d a2 = %d a3 = %d",a1,a2,a3)
 }

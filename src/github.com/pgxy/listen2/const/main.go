d5fc2d03d72047bdfa12dd7775dea285d63db2b4
tddey01 <kevinlandun@gmail.com>
2019-10-31 11:40:25 +0800

    常量

--- src/github.com/pgxy/listen2/const/main.go
+++ src/github.com/pgxy/listen2/const/main.go
@@ -8,9 +8,15 @@ func main() {
 	// const  a int  = 100  	 // 常量定义 必须赋值
 	// const b string = "hello"
 
+	// const (
+	// 	a  int = 100
+	// 	b string = "hello"    // 优雅写法
+	// )
 	const (
-		a  int = 100
-		b string = "hello"    // 优雅写法
+		a int = 100
+		b
+		c int = 200
+		d
 	)
 
 	// const (  // 更加专业写法
@@ -19,9 +25,10 @@ func main() {
 	// 	c
 	// )
 	// const (
-	// 	a = 1 << iota
+	// 	a = 1 << iota  // iota 值为 0
 	// 	b 
 	// 	c
 	// )
-	fmt.Printf("a = %d b = %s \n",a,b)
+
+	fmt.Printf("a = %d b = %d  c = %d d = %d \n",a,b,c,d)
 }

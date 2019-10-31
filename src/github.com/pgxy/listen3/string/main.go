a42fc33cf391451441b59ded0958486c7a002bcd
tddey01 <kevinlandun@gmail.com>
2019-10-31 17:56:49 +0800

    字符串练习第二版本

--- src/github.com/pgxy/listen3/string/main.go
+++ src/github.com/pgxy/listen3/string/main.go
@@ -4,30 +4,28 @@ import (
 	"fmt"
 )
 
-func testString(){
+func testString() {
 	var str = "hello"
 	// fmt.Printf("str[0] = %c",str[0])
-	fmt.Printf("str[0] = %c len(str) = %d\n",str[0],len(str))
+	fmt.Printf("str[0] = %c len(str) = %d\n", str[0], len(str))
 
 	for index, val := range str {
 		// fmt.Printf("str[%d] = %d\n",index,val)
-		fmt.Printf("str[%d] = %c\n",index,val)
+		fmt.Printf("str[%d] = %c\n", index, val)
 	}
 
 	// str[0] = '0' // 错误
 	// fmt.Printf("after modify:" , str)
-  
+
 	// 转成byte类型 修改字符串数值
 	var byteSlice [] byte
 	byteSlice = []byte(str)
 	byteSlice[0] = '0'
 	str = string(byteSlice)
 
-	fmt.Println("after modfiy: ",str)
+	fmt.Println("after modfiy: ", str)
 
-	fmt.Printf("len(str) = %d\n",len(str))
-
-
+	fmt.Printf("len(str) = %d\n", len(str))
 
 	str = "hello, 少林之巅"
 	fmt.Printf("len(str)=%d\n", len(str))
@@ -43,13 +41,13 @@ func testString(){
 	fmt.Printf("str 长度:%d, len(str)=%d\n", len(runeSlice), len(str))
 }
 
-func testReverseStringV1(){
-	var str = "hello"
-	var  bytes []byte = []byte(str)
+func testReverseStringV1() {
+	var str = "hello中文"
+	var bytes []byte = []byte(str)
 
-	for i := 0; i < len(str)/2 ; i++ {
-		tmp := bytes[len(str) - i -1]
-		bytes[len(str) - i -1] = bytes[i]
+	for i := 0; i < len(str)/2; i++ {
+		tmp := bytes[len(str)-i-1]
+		bytes[len(str)-i-1] = bytes[i]
 		bytes[i] = tmp
 	}
 
@@ -57,8 +55,22 @@ func testReverseStringV1(){
 	fmt.Println(str)
 }
 
+func testReverseStringV2() {
+	var str = "hello中文 "
+	var r []rune = []rune(str)
 
-func main(){
+	for i := 0; i < len(r)/2; i++ {
+		tmp := r[len(r)-i-1]
+		r[len(r)-i-1] = r[i]
+		r[i] = tmp
+	}
+
+	str = string(r)
+	fmt.Println(str)
+}
+
+func main() {
 	// testString()
-	testReverseStringV1()
+	// testReverseStringV1()
+	testReverseStringV2()
 }

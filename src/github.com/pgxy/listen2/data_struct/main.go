3e4722475698d97ab20016a567f54cfbfcf20240
tddey01 <kevinlandun@gmail.com>
2019-10-31 12:32:53 +0800

    && 和||  与 或使用方法

--- src/github.com/pgxy/listen2/data_struct/main.go
+++ src/github.com/pgxy/listen2/data_struct/main.go
@@ -12,4 +12,22 @@ func main() {
 	a = !a
 	fmt.Println(a)
 
+	var b bool = true
+	if a == true && b == true { //与
+		fmt.Println("right")
+	} else {
+		fmt.Println("not right")
+	}
+
+	if a == true || b == true { // 或
+		fmt.Println("|| right")
+	} else {
+		fmt.Println("||  not right")
+	}
 }
+
+//1. 布尔类型
+//a. var b bool 和 var b bool = true 和 var b = false
+//b. 操作符==和 != c. 取反操作符: !b
+//d. &&和||操作符
+//e. 格式化输出占位符: %t

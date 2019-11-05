3453a4da409cbf79d44fbd459a2fe0ae00d63d55
access <kevinlandun@gmail.com>
2019-11-05 16:14:10 +0800

    for循环 continue

--- src/github.com/pgxy/listen4/for/main.go
+++ src/github.com/pgxy/listen4/for/main.go
@@ -21,7 +21,27 @@ func testFor2() {
 	fmt.Printf("final:i=%d\n", i)
 }
 
+func testFor3() {
+	var i int
+	for i = 1; i <= 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Printf("i-%d\n", i)
+	}
+	fmt.Printf("final:i=%d\n", i)
+}
+
+func testFor4() {
+	i := 1
+	for i <= 10 {
+		fmt.Printf("i=%d\n", i)
+		i = i + 2
+	}
+}
 func main() {
 	//testFor1()
-	testFor2()
+	// testFor2()
+	// testFor3()
+	testFor4()
 }

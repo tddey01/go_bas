5c8669c86204af10cbe513a7c3181d03701d14c5
access <kevinlandun@gmail.com>
2019-11-05 14:25:34 +0800

    for循环 break

--- src/github.com/pgxy/listen4/for/main.go
+++ src/github.com/pgxy/listen4/for/main.go
@@ -2,11 +2,26 @@ package main
 
 import "fmt"
 
-func testFor2() {
-	for i := 1; i <= 10; i++ {
+func testFor1() {
+	var i int
+	for i = 1; i <= 10; i++ {
 		fmt.Printf("i-%d\n", i)
 	}
+	fmt.Printf("final:i=%d\n", i)
 }
+
+func testFor2() {
+	var i int
+	for i = 1; i <= 10; i++ {
+		if i > 5 {
+			break
+		}
+		fmt.Printf("i-%d\n", i)
+	}
+	fmt.Printf("final:i=%d\n", i)
+}
+
 func main() {
+	//testFor1()
 	testFor2()
 }

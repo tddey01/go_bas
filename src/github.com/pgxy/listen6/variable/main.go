c6af32639e319046f248dcb168e1c356539d4e56
access <kevinlandun@gmail.com>
2019-11-06 18:50:49 +0800

    语句句块内定义。

--- src/github.com/pgxy/listen6/variable/main.go
+++ src/github.com/pgxy/listen6/variable/main.go
@@ -12,6 +12,21 @@ func testGlobalVariable() {
 func testlocalVariable() {
 	var b int = 100
 	fmt.Printf("a=%d b=%d\n", a, b)
+
+	if b == 100 {
+		var c int = 100
+		fmt.Printf("c=%d\n", c)
+	}
+	if d := 100; d > 0 {
+		fmt.Printf("d=%d", d)
+	} else {
+		fmt.Printf("else d=%d", d)
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("for i=%d\n", i)
+	}
+
 }
 
 func main() {

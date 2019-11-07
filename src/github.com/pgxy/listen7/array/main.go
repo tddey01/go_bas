7351c988816b26fb5df12d29396e737d20aaa36e
access <kevinlandun@gmail.com>
2019-11-07 15:48:30 +0800

    数组⻓长度是类型的⼀一部分

--- src/github.com/pgxy/listen7/array/main.go
+++ src/github.com/pgxy/listen7/array/main.go
@@ -10,13 +10,52 @@ func testArrya1() {
 	a[3] = 500
 	a[4] = 600
 	fmt.Println(a)
+	//数组初始化
 }
 func testArrya2() {
 	var a [5]int = [5]int{10, 20, 30}
 	fmt.Println(a)
+	//定义时数组初始化
 }
 
+func testArrya3() {
+	a := [5]int{10, 20, 30}
+	fmt.Println(a)
+	//定义时数组初始化
+}
+
+func testArrya4() {
+	a := [...]int{10, 20, 30}
+	fmt.Println(a)
+	//定义时数组初始化
+}
+
+func testArrya5() {
+	a := [3]int{10, 3, 5}
+	fmt.Println(a)
+	//定义时数组初始化
+}
+
+func testArrya6() {
+	a := [5]int{3: 100, 4: 200}
+	fmt.Println(a)
+	//定义时数组初始化
+}
+
+func testArrya7() {
+	a := [5]int{3: 100, 4: 500}
+	fmt.Println(a)
+	var b [5]int
+	b = a
+	fmt.Println(b)
+	// 数组⻓长度是类型的⼀一部分
+}
 func main() {
 	//testArrya1() //数组初始化
-	testArrya2()
+	// testArrya2() //定义时数组初始化
+	// testArrya3() //定义时数组初始化
+	// testArrya4() //定义时数组初始化
+	//testArrya5() //定义时数组初始化
+	//testArrya6()
+	testArrya7() //数组⻓长度是类型的⼀一部分
 }

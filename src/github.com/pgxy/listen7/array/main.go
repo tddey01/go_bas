8493796fca974aa2b26b0cb2ef7b394857485047
access <kevinlandun@gmail.com>
2019-11-07 14:44:01 +0800

    数组初始化

---
+++ src/github.com/pgxy/listen7/array/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func testArrya1() {
+	var a [5]int
+	a[0] = 200
+	a[1] = 300
+	a[2] = 400
+	a[3] = 500
+	a[4] = 600
+	fmt.Println(a)
+}
+func main() {
+	testArrya1()
+}

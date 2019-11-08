9651da373acdd2f69a8a4b208b07f5ffa029e141
access <kevinlandun@gmail.com>
2019-11-08 12:29:14 +0800

    找出数组中和为给定值的两个元素的下标，⽐比如数组:[1,3,5,8,7]，找出两个元素 之和等于8的下标分别是(0, 4)和(1,2)。

--- src/github.com/pgxy/listen7/example/main.go
+++ src/github.com/pgxy/listen7/example/main.go
@@ -31,9 +31,28 @@ func testArraySum() {
 	}
 	sum := sumArray(b)
 	fmt.Println(sum)
+	fmt.Printf("sum=%d\n", sum)
+}
+
+func TwoSum(a [5]int, target int) {
+	for i := 0; i < len(a); i++ {
+		other := target - a[i]
+		for j := 0; j < len(a); j++ {
+			if a[j] == other {
+				fmt.Printf("(%d %d)\n", i, j)
+			}
+		}
+	}
+}
+func testTwoSum() {
+	//var b [5]int = [5]int{1,3,5,8,7}
+	//b := [5]int{1,3,5,8,7}
+	//b := [...]int{1, 3, 5, 8, 7}
+	b := [...]int{1, 3, 5, 6, 7}
+	TwoSum(b, 8)
 }
 
 func main() {
-	testArraySum()
-
+	//testArraySum()
+	testTwoSum()
 }

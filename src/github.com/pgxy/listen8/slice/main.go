dfff6f9d450c7920cd5ddaa2e6c6dfd77760a38a
access <kevinlandun@gmail.com>
2019-11-08 14:17:43 +0800

    切⽚片基本操作

--- src/github.com/pgxy/listen8/slice/main.go
+++ src/github.com/pgxy/listen8/slice/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func testSlice0() {
-	var a []int
+	var a []int //定义⼀一个int类型的空切⽚片
 	if a == nil {
 		fmt.Printf("a is nil\n")
 	} else {
@@ -17,7 +17,7 @@ func testSlice0() {
 func testSlice1() {
 	a := [5]int{1, 2, 3, 4, 5}
 	var b []int
-	b = a[1:4]
+	b = a[1:4] //基于数组a 创建⼀一个切⽚片，包括元素a[1] a[2] a[3]
 	fmt.Printf("slice b:%v\n", b)
 	fmt.Printf("b[0]=%d\n", b[0])
 	fmt.Printf("b[1]=%d\n", b[1])
@@ -26,7 +26,7 @@ func testSlice1() {
 }
 
 func testSlice2() {
-	a := []int{1, 2, 3, 4, 5}
+	a := []int{1, 2, 3, 4, 5} // 创建⼀一个数组并返回⼀一个切⽚片
 
 	fmt.Printf("slice a:%v type of a:%T\n", a, a)
 }
@@ -46,13 +46,18 @@ func testSlice3() {
 	// e  := a[0:len(a)]
 	e := a[:]
 	fmt.Printf("slice e:%v\n", e)
+
+	//a) arr[start:end]:包括start到end-1(包括end-1)之间的所有元素
+	//b) arr[start:]:包括start到arr最后⼀一个元素(包括最后⼀一个元素)之间的所有元素
+	//c) arr[:end]:包括0到end-1(包括end-1)之间的所有元素
+	//d) arr[:]:包括整个数组的所有元素
 }
 
 func testSlice4() {
-	a := [...]int{1, 2, 3, 4, 5, 7, 8, 9, 11}
+	a := [...]int{1, 2, 3, 4, 5, 7, 8, 9, 11} // 创建⼀一个数组，其中[...]是编译器器确定数组的⻓长度,darr的⻓长度是
 
 	fmt.Printf("array a:%v type of a:%T\n", a, a)
-	b := a[2:5]
+	b := a[2:5] //基于darr创建⼀一个切⽚片dslice,包括darr[2],darr[3],darr[4]三个元素
 	fmt.Printf("slice b:%v type of b:%T\n", b, b)
 	/*
 		b[0] = b[0] + 10
@@ -65,13 +70,16 @@ func testSlice4() {
 		}
 	*/
 	for index := range b {
+		//对于dslice中每个元素进⾏行行+1，其实修改是darr[2],darr[3],darr[4]
 		b[index] = b[index] + 10
 	}
 	fmt.Printf("after modify slice b, array a:%v type of a:%T\n", a, a)
 }
 
 func testSlice5() {
+	//切⽚片修改
 	a := [...]int{1, 2, 3}
+	//创建⼀一个切⽚片，包含整个数组的所有元素
 	s1 := a[:]
 	s2 := a[:]
 	s1[0] = 100

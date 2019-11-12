6b7b151584d83c62e0934b3db6c59c6cefb6c6b6
access <kevinlandun@gmail.com>
2019-11-12 15:06:57 +0800

    指针变量 切片 make初始化指针

--- src/github.com/pgxy/listen9/point_type/main.go
+++ src/github.com/pgxy/listen9/point_type/main.go
@@ -37,9 +37,35 @@ func testPoint3() {
 	modify(p)
 	fmt.Printf("b:%d\n", b)
 }
+
+func modify_arr(a *[3]int) {
+	(*a)[0] = 100 // 下标操作
+}
+func testPoint4() {
+	var b [3]int = [3]int{1, 3, 2}
+	modify_arr(&b)
+	fmt.Printf("b:%v\n", b)
+	//指针变量量传参示例例2
+}
+
+func testPoint5() {
+	var a *int = new(int)
+	*a = 100
+	fmt.Printf("a=:%d\n", *a)
+
+	var b *[]int = new([]int)
+	fmt.Printf("*b = %v\n", *b)
+	// (*b)[0] =  100 错误的切片
+	(*b) = make([]int, 5, 100)
+	(*b)[0] = 100
+	(*b)[1] = 200
+	fmt.Printf("*b :%v\n", *b)
+}
+
 func main() {
 	//testPoint1()
 	// testPoint2()
-	testPoint3() //指针变量量传参
-
+	// testPoint3() //指针变量量传参
+	// testPoint4() //指针变量量传参示例例2
+	testPoint5()
 }

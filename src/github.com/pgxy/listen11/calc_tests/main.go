f44322286c7b27982001899808dd9900decf3dbf
access <kevinlandun@gmail.com>
2019-11-13 15:09:18 +0800

    除了了可执⾏行行程序之外，⽤用户可以写⾃自定义包，⾃自定义包编译成静态库 导出变量量或函数。⾸首字⺟母⼤大写表示可导出，⼩小写表示私有。不不能被外部的包访问 go run运⾏行行go代码， 如果有多个⽂文件，需要把所有⽂文件都写到go run后⾯面 go build 编译go代码，如果是可执⾏行行程序，默认会在当前⽬目录⽣生成可执⾏行行 程序，可以使⽤用-o指定可执⾏行行程序⽣生成的⽬目录。 go install编译go代码，并且把可执⾏行行程序拷⻉贝到GOPATH的bin⽬目录，⾃自定义或 第三⽅方包会拷⻉贝到GOPATH的pkg⽬目录

---
+++ src/github.com/pgxy/listen11/calc_tests/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pgxy/listen11/calc"
+)
+
+func main() {
+	var sum = calc.Add(2, 5)
+	fmt.Printf("sum = %d", sum)
+}
+
+//go run运⾏行行go代码， 如果有多个⽂文件，需要把所有⽂文件都写到go run后⾯面
+//go build 编译go代码，如果是可执⾏行行程序，默认会在当前⽬目录⽣生成可执⾏行行 程序，可以使⽤用-o指定可执⾏行行程序⽣生成的⽬目录
+//go install编译go代码，并且把可执⾏行行程序拷⻉贝到GOPATH的bin⽬目录，⾃自定义或 第三⽅方包会拷⻉贝到GOPATH的pkg⽬目录

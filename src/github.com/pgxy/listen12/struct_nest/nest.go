075340b99638d236f24846302a3170a0fca23879
access <kevinlandun@gmail.com>
2019-11-15 16:29:03 +0800

    结构体初始化

---
+++ src/github.com/pgxy/listen12/struct_nest/nest.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	Province string
+	City     string
+}
+
+//
+//type User struct {
+//	Username string
+//	Sex      string
+//	address  Address
+//}
+//
+//func main() {
+//	user := &User{
+//		Username: "user01",
+//		Sex:      "man",
+//		address: Address{
+//			Province: "beijing",
+//			City:     "beijing",
+//		},
+//	}
+//
+//	fmt.Printf("user=%#v\n", user)
+//}
+
+type User struct {
+	Username string
+	Sex      string
+	address  *Address
+}
+
+func main() {
+	user := &User{
+		Username: "user01",
+		Sex:      "man",
+		address: &Address{
+			Province: "beijing",
+			City:     "beijing",
+		},
+	}
+
+	fmt.Printf("user=%#v\n", user)
+}

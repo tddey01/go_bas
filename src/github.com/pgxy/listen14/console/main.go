c9f6d3b110f2d59fb1c30a7fb0f1253571a4d377
access <kevinlandun@gmail.com>
2019-11-26 14:29:32 +0800

    从字符串串中获取输⼊入

--- src/github.com/pgxy/listen14/console/main.go
+++ src/github.com/pgxy/listen14/console/main.go
@@ -12,4 +12,11 @@ func main() {
 	os.Stdout.WriteString(string(buf[:]))
 	fmt.Scanf()
 	s = false
+	/*
+		终端其实是⼀一个⽂文件
+		终端相关⽂文件的实例例
+		os.Stdin:标准输⼊入的⽂文件实例例，类型为*File
+		os.Stdout:标准输出的⽂文件实例例，类型为*File
+		os.Stderr:标准错误输出的⽂文件实例例，类型为*File
+	*/
 }

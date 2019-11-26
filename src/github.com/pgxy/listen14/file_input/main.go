dfda9bb4007c7cbb602e9219515b61a67bf3e38c
access <kevinlandun@gmail.com>
2019-11-26 14:30:18 +0800

    以⽂文件的⽅方式操作终端 终端读取: File.Read(b []byte) 终端输出: File.Write(b []byte) File.WriteString(str string)

--- src/github.com/pgxy/listen14/file_input/main.go
+++ src/github.com/pgxy/listen14/file_input/main.go
@@ -14,12 +14,11 @@ func main() {
 	//fmt.Scanf("%d%s%f", &a, &b, &c)
 	//fmt.Println(a, b, c)
 	fmt.Fprintln(os.Stdout, "stdout", a, b, c)
-
 	/*
-		终端其实是⼀一个⽂文件
-		终端相关⽂文件的实例例
-		os.Stdin:标准输⼊入的⽂文件实例例，类型为*File
-		os.Stdout:标准输出的⽂文件实例例，类型为*File
-		os.Stderr:标准错误输出的⽂文件实例例，类型为*File
+	   以⽂文件的⽅方式操作终端
+	   终端读取:
+	   File.Read(b []byte)
+	   终端输出:
+	   File.Write(b []byte) File.WriteString(str string)
 	*/
 }

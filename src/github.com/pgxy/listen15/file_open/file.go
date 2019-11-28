2880f40ad50418adb39ae37c1583040e67ae465e
access <kevinlandun@gmail.com>
2019-11-28 14:36:57 +0800

    文件读取, file.Read和file.ReadAt。读到文件末尾返回:io.EOF

--- src/github.com/pgxy/listen15/file_open/file.go
+++ src/github.com/pgxy/listen15/file_open/file.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -13,4 +14,24 @@ func main() {
 		return
 	}
 	defer file.Close()
+
+	// var buf [128]byte
+	// 	n, err := file.Read(buf[:])
+	// 	fmt.Println("read length:", n)
+	// 	fmt.Println("data:",string(buf[0:n]))
+	var content []byte
+	var buf [128]byte
+	for {
+		n, err := file.Read(buf[:])
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("read file:", err)
+			return
+		}
+		content = append(content, buf[:n]...)
+	}
+	fmt.Println(string(content))
+
 }

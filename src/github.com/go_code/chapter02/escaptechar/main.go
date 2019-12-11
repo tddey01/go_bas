ff0906b590a157d92e81bfbcba4b6730060a72d3
access <kevinlandun@gmail.com>
2019-12-11 18:12:27 +0800

    ss
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ src/github.com/go_code/chapter02/escaptechar/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt" // fmt 包中提供格式化 输出 输入的函数
+func main() {
+	// 演示转义字符的使用
+	// fmt.Println("tom\tjack")
+	// fmt.Println("tom\njack")                                                               //换行符
+	// fmt.Println("\\Users\\access\\go\\src\\github.com\\go_code\\chapter02\\escaptechar\\") //
+	// fmt.Println("tom\"jack\" hel")                                                         // 转义
+	// fmt.Println("tom\rjack")
+	fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京") // 课堂练习
+}

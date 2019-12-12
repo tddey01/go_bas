e299a5496968785e5aec5f2c75231a57030e0698
access <kevinlandun@gmail.com>
2019-12-12 17:14:34 +0800

    全局变量方式
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter03/demo03/main.go
+++ src/github.com/go_code/chapter03/demo03/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+// 定义全局变量
+var n4 = 100
+var n5 = 200
+var name1 = "jack"
+
+// 上面的声明方式 也可以改成一次性声明
+var (
+	m1    = 300
+	m2    = 900
+	name2 = "mary"
+)
+
 func main() {
 	//该案案例演示golang如何一次性声明多个变量
 	var n1, n2, n3 int
@@ -12,6 +24,11 @@ func main() {
 	fmt.Println("s1=", s1, "name=", name, "s2=", s2)
 
 	// 一次性声明多个变量方式3 同样可以使用类型推导
-	a1, a2, a3 := 100, "kn",999
-	fmt.Println("a1=",a1, "a2=",a2, "a3=",a3)
+	a1, a2, a3 := 100, "kn", 999
+	fmt.Println("a1=", a1, "a2=", a2, "a3=", a3)
+
+	//输出全局变量
+	fmt.Println("n4=", n4, "n5=", n5, "name=", name1)
+
+	fmt.Println("m1=", m1, "m2=", m2, "name2=", name2)
 }

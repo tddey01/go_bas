3fabb85f99802696d068b7d697143ee19b9d5742
access <kevinlandun@gmail.com>
2019-12-17 14:14:41 +0800

    基本数据类型转成string使用
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ src/github.com/go_code/chapter03/basictype2string12/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	_ "fmt"
+	"strconv"
+	_ "unsafe"
+)
+
+// Golang 中基本数据类型转成string使用
+
+func main() {
+
+	var num1 int = 99
+	var num2 float64 = 23.456
+	var b bool = true
+	var myChar byte = 'h'
+
+	var str string // 空的str
+
+	// 第一种方式转换 fmt.Sprintf方法
+	str = fmt.Sprintf("%d", num1)
+	fmt.Printf("str type %T str=%q\n", str, str)
+
+	str = fmt.Sprintf("%f", num2)
+	fmt.Printf("str type %T str=%q\n", str, str)
+
+	str = fmt.Sprintf("%t", b)
+	fmt.Printf("str type %T str=%q\n", str, str)
+
+	str = fmt.Sprintf("%c", myChar)
+	fmt.Printf("str type %T str=%q\n", str, str)
+
+	//第二种方式 strconv 函数
+	var num3 int = 99
+	var num4 float64 = 23.456
+	var b2 bool = true
+
+	str = strconv.FormatInt(int64(num3), 10)
+	fmt.Printf("str type %T str=%q\n", str, str)
+
+	str = strconv.FormatFloat(num4, 'f', 10, 64)
+	// 说明 ’f' 格式 10： 表示小数点保留10位， 64：表示这个小数是float64位
+	fmt.Printf("str type %T str=%q\n", str, str)
+
+	str = strconv.FormatBool(b2)
+	fmt.Printf("str type %T  str=%q\n", str, str)
+}

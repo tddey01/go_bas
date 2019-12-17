86ef1a06a1b27c4c86ad7b37a7b91032285dd4a0
access <kevinlandun@gmail.com>
2019-12-17 18:40:44 +0800

    使用utils.go 文件的变量或者函数， 我们需要先引入该包
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter03/demo04/main.go
+++ src/github.com/go_code/chapter03/demo04/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	// 为了使用utils.go 文件的变量或者函数， 我们需要先引入该包
+	"github.com/go_code/chapter03/demo04/model"
 )
 
 // 变量使用的注意事项
@@ -14,5 +16,8 @@ func main() {
 	// i = 1.2 // int， 原因是不能改变数据类型
 
 	//变量在同一个作用域(在一个函数或者在代码块)内不能重名
+
+	// 我们使用utils.go 的heeroName 包名.标识符
+	fmt.Println(model.HeroName)
 
 }

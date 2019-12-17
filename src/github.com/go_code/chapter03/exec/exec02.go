069fabf32beab5adccac44d724f6bcbdaf13fe68
access <kevinlandun@gmail.com>
2019-12-17 17:00:25 +0800

    指针课堂练习
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter03/exec/exec02.go
+++ src/github.com/go_code/chapter03/exec/exec02.go
@@ -7,5 +7,18 @@ import (
 func main() {
 
 	var num int = 9
-	fmt.Printf("num address=%v\n", num)
+	fmt.Printf("num address=%v\n", &num)
+
+	var ptr *int
+	ptr = &num
+	*ptr = 10 // 这里修改时 会到num的值变化
+	fmt.Printf("num=%v\n", num)
+
+	var a int = 300
+	var b int = 400
+	var prt *int = &a
+	*prt = 100
+	prt = &b
+	*prt = 200
+	fmt.Printf("a=%d b=%d *prt=%d\n", a, b, *prt)
 }

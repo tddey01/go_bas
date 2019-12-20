9d68525159d83e1255e3c8ff428786fe02678675
tddey <liujicun01@gmail.com>
2019-12-20 20:37:14 +0800

    练习
    Signed-off-by: tddey <liujicun01@gmail.com>

--- src/github.com/go_code/chapter04/exec/main.go
+++ src/github.com/go_code/chapter04/exec/main.go
@@ -8,6 +8,11 @@ func main() {
 	var week int = days / 7
 	var day int = days % 7
 	fmt.Printf("%d个星期零%d天\n", week, day)
-	fmt.Println("helo")
+
+	//  定义一个变量报错华氏温度，华氏温度转换摄氏温度的公式为
+	// 5、9（华氏温度-100），请求出华氏温度对应摄氏温度
+	var huashi float32 = 134.2
+	var sheshi float32 = 5.0 / 9 * (huashi - 100)
+	fmt.Printf("%v 对应摄氏温度=%v \n", huashi, sheshi)
 
 }

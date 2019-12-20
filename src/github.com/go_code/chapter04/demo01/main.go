ab1b0b1c07f2445762470524913ea629312bd247
tddey <liujicun01@gmail.com>
2019-12-20 13:21:08 +0800

    除法运算

--- src/github.com/go_code/chapter04/demo01/main.go
+++ src/github.com/go_code/chapter04/demo01/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 重点 讲解 / 、 %
+	// 说明： 如果运算的数都是整数  那么除后 去掉小数部分 ， 保留整数
+	fmt.Println(10 / 4)
+
+	var n1 float32 = 10 / 4 //
+	fmt.Println(n1)
+	// 希望保留小数部分， 则需要有浮点数参与运算
+	var n2 float32 = 10.0 / 4
+	fmt.Println(n2)
+}

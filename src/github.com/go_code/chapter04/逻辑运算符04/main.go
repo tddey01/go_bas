9d56bfb20468b972673aa081f66fc19fe3e7deeb
tddey <liujicun01@gmail.com>
2019-12-23 20:14:13 +0800

    案例演示
    Signed-off-by: tddey <liujicun01@gmail.com>

--- src/github.com/go_code/chapter04/逻辑运算符04/main.go
+++ src/github.com/go_code/chapter04/逻辑运算符04/main.go
@@ -2,31 +2,51 @@ package main
 
 import "fmt"
 
+// 声明一个函数（测试）
+func test() bool {
+	fmt.Println("test....")
+	return true
+}
+
 func main() {
-	// 逻辑运算符的使用   &&  与
-	var age int = 40
-	if age > 30 && age < 50 {
-		fmt.Println("ok1")
+
+	var i int = 10
+	// 短路与
+	// 说明 因为 i<9 为false  因此后面的test() 就不执行了
+	if i < 9 && test() {
+		fmt.Println("ok...")
 	}
 
-	if age > 30 && age < 40 {
-		fmt.Println("ok2")
+	// 说明 因为 i<9 为true  因此后面的test() 就不执行了
+	if i < 9 || test() {
+		fmt.Println("hello...")
 	}
+	/*
+		// 逻辑运算符的使用   &&  与
+		var age int = 40
+		if age > 30 && age < 50 {
+			fmt.Println("ok1")
+		}
 
-	// 逻辑运算符的使用   ||  或
-	if age > 30 || age < 50 {
-		fmt.Println("ok3")
-	}
+		if age > 30 && age < 40 {
+			fmt.Println("ok2")
+		}
 
-	if age > 30 || age < 40 {
-		fmt.Println("ok4")
-	}
+		// 逻辑运算符的使用   ||  或
+		if age > 30 || age < 50 {
+			fmt.Println("ok3")
+		}
 
-	// 逻辑运算符的使用 ！
-	if age > 30 {
-		fmt.Println("ok5")
-	}
-	if !(age > 30) {
-		fmt.Println("ok6")
-	}
+		if age > 30 || age < 40 {
+			fmt.Println("ok4")
+		}
+
+		// 逻辑运算符的使用 ！
+		if age > 30 {
+			fmt.Println("ok5")
+		}
+		if !(age > 30) {
+			fmt.Println("ok6")
+		}
+	*/
 }

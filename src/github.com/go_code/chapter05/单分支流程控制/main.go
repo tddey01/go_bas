a9397c8804bf5482c736f79846f46333d7dba6cf
tddey <liujicun01@gmail.com>
2019-12-28 12:36:36 +0800

    if 单分支控制流程
    Signed-off-by: tddey <liujicun01@gmail.com>

--- src/github.com/go_code/chapter05/单分支流程控制/main.go
+++ src/github.com/go_code/chapter05/单分支流程控制/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func main(){
+func main() {
 	// ifDemo.go
 	// 编写一个 可以输入年龄 如果该同志年龄大于18 输出 你年龄大于18， 要对自己负责
 
@@ -14,12 +14,17 @@ func main(){
 	 2 从控制台接收一个输入 fmt.Scanlne(&age)
 	 3 if 判断
 	*/
-	
-	var  age  int 
-	fmt.Println("请输入年龄:")
-	fmt.Scanln(&age)
 
-	if age > 18 {
+	// var  age  int
+	// fmt.Println("请输入年龄:")
+	// fmt.Scanln(&age)
+
+	// if age > 18 {
+	// 	fmt.Println("你年龄大于18， 要对自己负责")
+	// }
+
+	// golang 支持在if中 直接定义一个变量
+	if age := 20; age > 18 {
 		fmt.Println("你年龄大于18， 要对自己负责")
 	}
 }

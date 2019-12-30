1cc3355ad559436ae7b46ae876cc6d4802f1e240
kevinlandun <kevinlandun@gmail.com>
2019-12-30 15:39:16 +0800

    if多语句 判断
    Signed-off-by: kevinlandun <kevinlandun@gmail.com>

---
+++ src/github.com/go_code/chapter05/exec/exec02/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//分析思路
+	// score 分数变量 int
+	// 选择多分支流程控制
+	// 键盘输入成绩 fmt.Scanln
+
+	var score int
+	fmt.Println("请输入成绩:")
+	fmt.Scanln(&score)
+
+	// 多分支判断
+	if score == 100 {
+		fmt.Println("奖励一辆宝马一台")
+	} else if score > 80 && score <= 99 {
+		fmt.Println("奖励一台iPhone7")
+	} else if score >= 60 && score <= 80 {
+		fmt.Println("奖励一个ipad")
+	} else {
+		fmt.Println("什么都不奖励")
+	}
+}

51533ba90d409ddd1b5d49b11f7ada486c9f9d46
access <kevinlandun@gmail.com>
2020-01-06 16:25:50 +0800

    continue 案例
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ src/github.com/go_code/chapter05/continuedemo/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// continue案例
+	// 演示执行标签的形式来使用continue
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				continue
+				fmt.Println("ok")
+			}
+			fmt.Println("j=", j)
+		}
+
+	}
+}

c4eef08594c5080a08fac955104f46e9ca97ca7a
access <kevinlandun@gmail.com>
2020-01-06 16:41:01 +0800

    continue练习
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter05/continuedemo/main.go
+++ src/github.com/go_code/chapter05/continuedemo/main.go
@@ -6,14 +6,31 @@ func main() {
 	// continue案例
 	// 演示执行标签的形式来使用continue
 
-	for i := 0; i < 4; i++ {
-		for j := 0; j < 10; j++ {
-			if j == 2 {
-				continue
-				fmt.Println("ok")
-			}
-			fmt.Println("j=", j)
-		}
+	// for i := 0; i < 4; i++ {
+	// 	for j := 0; j < 10; j++ {
+	// 		if j == 2 {
+	// 			continue
+	// 			fmt.Println("ok")
+	// 		}
+	// 		fmt.Println("j=", j)
+	// 	}
+	// }
 
+	// for i := 0; i < 2; i++ {
+	// 	for j := 1; j < 4; j++ {
+	// 		if j == 2 {
+	// 			continue
+	// 		}
+	// 		fmt.Println("i=", i, "j=", j)
+	// 	}
+	// }
+here:
+	for i := 0; i < 2; i++ {
+		for j := 1; j < 4; j++ {
+			if j == 2 {
+				continue here
+			}
+			fmt.Println("i=", i, "j=", j)
+		}
 	}
 }

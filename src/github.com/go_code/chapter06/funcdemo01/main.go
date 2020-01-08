52d2d51a6318fe1d483ce519dbdc4711cafd2399
access <kevinlandun@gmail.com>
2020-01-08 16:57:34 +0800

    包使用入门导入
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter06/funcdemo01/main.go
+++ src/github.com/go_code/chapter06/funcdemo01/main.go
@@ -1,24 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func cal(n1 float64, n2 float64, operator byte) float64 {
-
-	var res float64
-	switch operator {
-	case '+':
-		res = n1 + n2
-	case '-':
-		res = n1 - n2
-	case '*':
-		res = n1 * n2
-	case '/':
-		res = n1 / n2
-	default:
-		fmt.Println("操作符号有误...")
-	}
-	return res
-}
+	"github.com/go_code/chapter06/funcdemo01/utils"
+)
 
 func main() {
 	//输入两个数  在输入一个运算符（+、-、*、/) 得到结果
@@ -44,6 +30,6 @@ func main() {
 	var n1 float64 = 1.2
 	var n2 float64 = 2.3
 	var operator byte = '-'
-	result := cal(n1, n2, operator)
+	result := utils.Cal(n1, n2, operator)
 	fmt.Println("res=", result)
 }
---
+++ src/github.com/go_code/chapter06/funcdemo01/utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import "fmt"
+
+//将计算的功能，放到一个函数中，然后在需要使用，调用即可
+//为了让其它包的文件使用Cal函数，需要将C大小类似其它语言的public
+func Cal(n1 float64, n2 float64, operator byte) float64 {
+
+	var res float64
+	switch operator {
+	case '+':
+		res = n1 + n2
+	case '-':
+		res = n1 - n2
+	case '*':
+		res = n1 * n2
+	case '/':
+		res = n1 / n2
+	default:
+		fmt.Println("操作符号有误...")
+	}
+	return res
+}

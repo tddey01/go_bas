74ccffd0c4838eab244be59de45d1ea9db078611
access <kevinlandun@gmail.com>
2020-01-13 21:35:43 +0800

    函数 指针调用
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter06/funcdemo04/main.go
+++ src/github.com/go_code/chapter06/funcdemo04/main.go
@@ -16,6 +16,7 @@ func test02(n1 int) {
 
 // n1  就是*int 类型 指针类型
 func test03(n1 *int) {
+	fmt.Printf("n1地址 %v\n", &n1)
 	*n1 = *n1 + 10
 	fmt.Println("test03() n1=", *n1)
 }
@@ -25,6 +26,7 @@ func main() {
 	// fmt.Println("main() num=", num)
 
 	num := 20
+	fmt.Println("main() num=", num) //30
 	test03(&num)
 	fmt.Println("main() num=", num) //30
 }

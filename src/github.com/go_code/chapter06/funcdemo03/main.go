9e668aff62787e4089785ce928a5b4456c910977
access <kevinlandun@gmail.com>
2020-01-13 14:23:09 +0800

    递归if判断
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter06/funcdemo03/main.go
+++ src/github.com/go_code/chapter06/funcdemo03/main.go
@@ -9,7 +9,18 @@ func test(n int) {
 	}
 	fmt.Println("n=", n)
 }
+
+func test2(n int) {
+	if n > 2 {
+		n--
+		test2(n)
+	} else {
+		fmt.Println("n=", n)
+	}
+}
+
 func main() {
 	// 看一段代码
-	test(4) // 通过分析来查看地轨调用的特点
+	// test(4) // 通过分析来查看地轨调用的特点
+	test2(4) // 通过分析来查看地轨调用的特点
 }

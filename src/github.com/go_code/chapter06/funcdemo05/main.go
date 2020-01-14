328b43a03a5ad23ee0a6d2966d4535efaea035ee
access <kevinlandun@gmail.com>
2020-01-14 13:39:13 +0800

    更新下
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter06/funcdemo05/main.go
+++ src/github.com/go_code/chapter06/funcdemo05/main.go
@@ -7,6 +7,11 @@ import "fmt"
 func getSum(n1 int, n2 int) int {
 	return n1 + n2
 }
+
+// 函数既然是一种数据类型 因此在Go中，函数可以作为参数 并且调用
+func myFun(funvar func(int, int) int, num1 int, num2 int) int {
+	return funvar(num1, num2)
+}
 func main() {
 
 	a := getSum
@@ -14,5 +19,9 @@ func main() {
 
 	res := a(10, 40) // 等价于 res := getSum(10,40)
 	fmt.Println("res=", res)
+
+	//案例
+	res2 := myFun(getSum, 50, 60)
+	fmt.Println("res2=", res2)
 
 }

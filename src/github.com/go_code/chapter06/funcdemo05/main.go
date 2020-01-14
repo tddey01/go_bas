e98c609d1a436e705204b307421b0ba1ed743ccd
access <kevinlandun@gmail.com>
2020-01-14 18:49:58 +0800

    可变参数
    Signed-off-by: access <kevinlandun@gmail.com>

--- src/github.com/go_code/chapter06/funcdemo05/main.go
+++ src/github.com/go_code/chapter06/funcdemo05/main.go
@@ -12,6 +12,33 @@ func getSum(n1 int, n2 int) int {
 func myFun(funvar func(int, int) int, num1 int, num2 int) int {
 	return funvar(num1, num2)
 }
+
+// 案例 2
+//这时 myFun 就是 func(int, int) 类型
+type myFunType func(int, int) int
+
+func myFun2(funvar myFunType, num1 int, num2 int) int {
+	return funvar(num1, num2)
+}
+
+// 支持对函数返回值命名
+func getSumAndSub(n1 int, n2 int) (sum int, sub int) {
+	sub = n1 - n2
+	sum = n1 + n2
+	return
+}
+
+// 编写一个sum函数
+// 可变参数的使用
+func sum(n1 int, args ...int) int {
+	sum := n1
+	//遍历args...
+	for i := 0; i < len(args); i++ {
+		sum += args[i] //args[0] 表示去除args 切片的第一个值  其他依次类推
+	}
+	return sum
+}
+
 func main() {
 
 	a := getSum
@@ -24,4 +51,24 @@ func main() {
 	res2 := myFun(getSum, 50, 60)
 	fmt.Println("res2=", res2)
 
+	// 给int 取别名 在go中 myInt和int虽然都是int类型  但是go认为myInt和int两个类型
+	type myInt int
+
+	var num1 myInt //
+	var num2 int
+	num1 = 40
+	num2 = int(num1) //注意这里依然需要显示转换go认为myInt和int是两个类型
+	fmt.Println("num1=", num1, "num2=", num2)
+
+	// 案例2
+	res3 := myFun2(getSum, 500, 600)
+	fmt.Println("res3=", res3)
+
+	//返回值命名 案例
+	a1, b1 := getSumAndSub(1, 2)
+	fmt.Printf("a1=%v b1=%v\n", a1, b1)
+
+	// 测试 可变参数使用
+	res4 := sum(10, 0, -1, 90, 10, 100)
+	fmt.Println("res4=", res4)
 }

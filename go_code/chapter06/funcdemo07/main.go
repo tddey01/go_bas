0fce413b9142c8b5579e36369f74ba76a9e339df
access <kevinlandun@gmail.com>
2020-01-15 13:10:28 +0800

    闭包
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter06/funcdemo07/main.go
+++ go_code/chapter06/funcdemo07/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // 累加器
 func AddUpeer() func(int) int {
@@ -14,6 +17,21 @@ func AddUpeer() func(int) int {
 	}
 }
 
+// 1)编写一个函数 makeSuffix(suffix string)  可以接收一个文件后缀名(比如.jpg)，并返回一个闭包
+// 2)调用闭包，可以传入一个文件名，如果该文件名没有指定的后缀(比如.jpg) ,则返回 文件名.jpg , 如果已经有.jpg后缀，则返回原文件名。
+// 3)要求使用闭包的方式完成
+// 4)strings.HasSuffix , 该函数可以判断某个字符串是否有指定的后缀。
+
+func makeSuffix(suffix string) func(string) string {
+	return func(name string) string {
+		// 如果 name 没有指定的后缀， 则加上  否则就返回原来的名字
+		if !strings.HasSuffix(name, suffix) {
+			return name + suffix
+		}
+		return name
+	}
+
+}
 func main() {
 	//使用前面的代码
 	f := AddUpeer()
@@ -21,4 +39,10 @@ func main() {
 	fmt.Println(f(1))
 	fmt.Println(f(2))
 	fmt.Println(f(3))
+
+	// 测试makeSuffix 使用
+	//返回闭包
+	f2 := makeSuffix(".jpg")
+	fmt.Println("文件名处理后=", f2("winter"))
+	fmt.Println("文件名处理后=", f2("bird.jpg"))
 }

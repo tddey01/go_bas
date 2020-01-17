16e3aff397928e6d59ac31e363fd62dcbba4a0bd
access <kevinlandun@gmail.com>
2020-01-17 13:08:45 +0800

    golang函数 日期函数
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ go_code/chapter06/timefunc 时间函数/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//看看日期和时间相关函数和方法使用
+	// 1 获取当前时间
+	now := time.Now()
+	fmt.Printf("now=%v now type=%T\n", now, now)
+	//now=2020-01-17 12:59:10.856011 +0800 CST m=+0.000063276 now type=time.Time
+
+	// 2 通过now可以获取到年月日 时分秒
+	fmt.Printf("年=%v\n", now.Year())
+	fmt.Printf("月=%v\n", now.Month())
+	fmt.Printf("月=%v\n", int(now.Month()))
+	fmt.Printf("日=%v\n", now.Day())
+	fmt.Printf("时=%v\n", now.Hour())
+	fmt.Printf("分=%v\n", now.Minute())
+	fmt.Printf("秒=%v\n", now.Second())
+}

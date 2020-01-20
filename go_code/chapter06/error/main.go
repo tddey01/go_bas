473c7cb5236ce2caf73c235756ae41970e929580
access <kevinlandun@gmail.com>
2020-01-20 13:32:27 +0800

    异常捕获处理
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter06/error/main.go
+++ go_code/chapter06/error/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func test() {
+	// 使用defer + recover 来捕获和处理异常
+	defer func() {
+		err := recover() // recover()内置函数， 可以捕获到异常
+		if err != nil {  // 说捕获异常错误
+			fmt.Println("err=", err)
+		}
+	}()
 	num1 := 10
 	num2 := 0
 	res := num1 / num2
@@ -11,5 +21,9 @@ func test() {
 func main() {
 	// 测试
 	test()
-	fmt.Println("main() 下吗代码...")
+	for {
+		fmt.Println("main() 下吗代码...")
+		time.Sleep(time.Second)
+	}
+
 }

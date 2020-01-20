18a35654dd623f6caebfcc960d8668768b1b28b2
access <kevinlandun@gmail.com>
2020-01-20 13:38:47 +0800

    异常错误捕获处理 的好处
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter06/error/main.go
+++ go_code/chapter06/error/main.go
@@ -8,15 +8,19 @@ import (
 func test() {
 	// 使用defer + recover 来捕获和处理异常
 	defer func() {
-		err := recover() // recover()内置函数， 可以捕获到异常
-		if err != nil {  // 说捕获异常错误
+		// err := recover() // recover()内置函数， 可以捕获到异常
+		// if err != nil {  // 说捕获异常错误
+		if err := recover(); err != nil { //这个写 正常
 			fmt.Println("err=", err)
+			// 这里就可以将错误信息发给管理员
+			fmt.Println("发送邮件给admin@sohu.com")
 		}
 	}()
 	num1 := 10
 	num2 := 0
 	res := num1 / num2
 	fmt.Println("res=", res)
+
 }
 func main() {
 	// 测试

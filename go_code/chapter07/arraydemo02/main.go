ab03f5c3537d3da4239c3163f3e6d6d074fcb52e
kn <root@localhost.com>
2020-01-21 22:36:55 +0800

    数组变量 地址可以快速完成数据交互

--- go_code/chapter07/arraydemo02/main.go
+++ go_code/chapter07/arraydemo02/main.go
@@ -12,4 +12,13 @@ func main() {
 	intArr[2] = 30
 	fmt.Printf("intarr的地址=%p intArr[0] 地址%p intArr[1] 地址%p \n", &intArr[0], &intArr[1], &intArr[2]) // 获取内存地址
 	//intarr的地址=0xc0000141a0 intArr[0] 地址0xc0000141a8 intArr[1] 地址0xc0000141b0
+
+	//从终端循环输入5个成绩， 保存到float64数组，并输出
+	var score [5]float64
+
+	for i :=0; i<len(score); i++{
+		fmt.Printf("请输入%d个元素的值",i+1)
+		fmt.Scanln(&score[i])
+		
+	}
 }

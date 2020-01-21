f0a8791fe046128c36b69020f606198e3da16b77
access <kevinlandun@gmail.com>
2020-01-21 21:00:53 +0800

    数组
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter07/arraydemo02/main.go
+++ go_code/chapter07/arraydemo02/main.go
@@ -7,5 +7,9 @@ func main() {
 	// 当我们定义完数组后，其实数组的各个元素有默认值 0
 	fmt.Println(intArr)
 
-	fmt.Printf("intarr的地址=%p intArr[0] 地址%p intArr[1] 地址%p \n", &intArr, &intArr[0], &intArr[1]) // 获取内存地址
+	intArr[0] = 10
+	intArr[1] = 20
+	intArr[2] = 30
+	fmt.Printf("intarr的地址=%p intArr[0] 地址%p intArr[1] 地址%p \n", &intArr[0], &intArr[1], &intArr[2]) // 获取内存地址
+	//intarr的地址=0xc0000141a0 intArr[0] 地址0xc0000141a8 intArr[1] 地址0xc0000141b0
 }

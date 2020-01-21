114cab67d6415430535cbacaf7da5985e81b9539
access <kevinlandun@gmail.com>
2020-01-21 19:26:19 +0800

    数组基本
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter07/arraydemo02/main.go
+++ go_code/chapter07/arraydemo02/main.go
@@ -1,7 +1,11 @@
 package main
-func main(){
-	var intArr [3]int
-	// 当我们定义完数组后，其实数组的各个元素有默认值
-	
 
+import "fmt"
+
+func main() {
+	var intArr [3]int // int 占用8个字节
+	// 当我们定义完数组后，其实数组的各个元素有默认值 0
+	fmt.Println(intArr)
+
+	fmt.Printf("intarr的地址=%p intArr[0] 地址%p intArr[1] 地址%p \n", &intArr, &intArr[0], &intArr[1]) // 获取内存地址
 }

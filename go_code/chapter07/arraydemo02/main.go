049d0d6a9b9d452f124c07e66a7d868864e4cf87
access <kevinlandun@gmail.com>
2020-01-22 13:25:57 +0800

    数组使用基本
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter07/arraydemo02/main.go
+++ go_code/chapter07/arraydemo02/main.go
@@ -18,10 +18,28 @@ func main() {
 
 	for i := 0; i < len(score); i++ {
 		fmt.Printf("请输入%d个元素的值", i+1)
-		fmt.Scanln(&score[i])
+		// fmt.Scanln(&score[i])
 	}
 	// 变量数组打印
 	for i := 0; i < len(score); i++ {
 		fmt.Printf("score[%d]=%v\n", i, score[i])
 	}
+
+	// 四种初始化数组的方式
+	var numArr01 [3]int = [3]int{1, 2, 3}
+	fmt.Println("numArr01=", numArr01)
+
+	var numArr02 = [3]int{5, 6, 7}
+	fmt.Println("numArr02=", numArr02)
+
+	// 这里[...]是规定的写法
+	var numArr03 = [...]int{7, 8, 9}
+	fmt.Println("numArr03=", numArr03)
+
+	var numArr04 = [...]int{1: 800, 0: 900, 2: 999}
+	fmt.Println("numArr04=", numArr04)
+
+	//类型推导
+	strArr05 := [...]string{1: "tom", 0: "jack", 2: "mary"}
+	fmt.Println("strArr05=", strArr05)
 }

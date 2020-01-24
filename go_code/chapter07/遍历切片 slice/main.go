f5110926ab603799719c57d123b3e9feef357e92
access <kevinlandun@gmail.com>
2020-01-24 12:52:23 +0800

    for -range 遍历切片
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter07/遍历切片 slice/main.go
+++ go_code/chapter07/遍历切片 slice/main.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	// 使用常规的for循环切片
 	var arr [5]int = [...]int{10, 20, 30, 40, 50}
-	slice := arr[1:4] // 20,30,40
+	// slice := arr[1:4] // 20,30,40
+	// slice := arr[0:len(arr)]
+	slice := arr[:] // 简写 两头都是默认值
 	for i := 0; i < len(slice); i++ {
 		fmt.Printf("slice[%v]=%v\n", i, slice[i])
 	}

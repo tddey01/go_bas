b0bee4d66ede34e5bd28cf8293fc345a3658fe16
kn <root@localhost.com>
2020-01-27 18:08:49 +0800

    二维数组 格式化输出

--- go_code/chapter08/multiarray/main.go
+++ go_code/chapter08/multiarray/main.go
@@ -1,4 +1,6 @@
 package main
+import "fmt"
+
 
 func main() {
 	// 二维数组的演示
@@ -9,5 +11,21 @@ func main() {
 		0 0 0 0 0 0
 	*/
 	// 定义 /声明  二维数组
+
 	var arr [4][6]int
+	// 赋初值
+	arr[1][2] = 1
+	arr[2][1] = 1
+	arr[2][3] = 1
+	
+	//遍历二维数组 按照要求输出图形
+	for i :=0;i<4;i++{
+		// fmt.Println(arr[i])
+		for  j :=0 ; j<6;j++{
+			fmt.Print(arr[i][j], " ")
+			// fmt.Print(arr[i][j], " ")
+		}
+		fmt.Println()
+	}
+	// fmt.Println(arr)
 }

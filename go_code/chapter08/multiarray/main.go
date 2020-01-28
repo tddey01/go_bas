9d3f3a376e158f9e095a274a1b4e61c2765fec12
tddey <liujicun01@gmail.com>
2020-01-28 15:32:05 +0800

    二维数组分析
    Signed-off-by: tddey <liujicun01@gmail.com>

--- go_code/chapter08/multiarray/main.go
+++ go_code/chapter08/multiarray/main.go
@@ -1,6 +1,6 @@
 package main
-import "fmt"
 
+import "fmt"
 
 func main() {
 	// 二维数组的演示
@@ -17,15 +17,29 @@ func main() {
 	arr[1][2] = 1
 	arr[2][1] = 1
 	arr[2][3] = 1
-	
+
 	//遍历二维数组 按照要求输出图形
-	for i :=0;i<4;i++{
+	for i := 0; i < 4; i++ {
 		// fmt.Println(arr[i])
-		for  j :=0 ; j<6;j++{
+		for j := 0; j < 6; j++ {
 			fmt.Print(arr[i][j], " ")
 			// fmt.Print(arr[i][j], " ")
 		}
 		fmt.Println()
 	}
 	// fmt.Println(arr)
+
+	var arr2 [2][3]int //以这个为例 分析arr2在内存的布局
+	arr2[1][1] = 10
+	fmt.Println(arr2)
+
+	fmt.Printf("arr2[0]的内存地址%p\n", &arr2[0])
+	fmt.Printf("arr2[1]的内存地址%p\n", &arr2[1])
+
+	fmt.Printf("arr2[0][0]的内存地址%p\n", &arr2[0][0])
+	fmt.Printf("arr2[1][1]的内存地址%p\n", &arr2[1][0])
+
+	fmt.Println()
+	var arr3 = [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	fmt.Println("arr3=", arr3)
 }

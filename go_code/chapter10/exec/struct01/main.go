714845e8101ea28a75b77d9859e94c627d0ad31e
access <kevinlandun@gmail.com>
2020-02-04 13:28:47 +0800

    案例测试 指针
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter10/exec/struct01/main.go
+++ go_code/chapter10/exec/struct01/main.go
@@ -17,7 +17,10 @@ func main() {
 	fmt.Println((*p2).Age) // 10
 	fmt.Println(p2.Age) //10
 	p2.Name = "tom"
-	fmt.Printf("p2.Name=%v p1.Name=%v\n", p2.Name, p1.Name) //p2.Name=tom p1.Name=tom
+	fmt.Printf("p2.Name=%v p1.Name=%v\n", p2.Name, p1.Name)    //p2.Name=tom p1.Name=tom
 	fmt.Printf("p2.Name=%v p1.Name=%v\n", (*p2).Name, p1.Name) //p2.Name=tom p1.Name=tom
+
+	fmt.Printf("p1的地址%p\n", &p1)
+	fmt.Printf("p2的地址%p p2的值%p\n", &p2, p2)
 
 }

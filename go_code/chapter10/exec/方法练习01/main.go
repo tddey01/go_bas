7d9a17c47d581204831bb20b50cd4e559b5358eb
access <kevinlandun@gmail.com>
2020-02-06 14:24:47 +0800

    练习
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter10/exec/方法练习01/main.go
+++ go_code/chapter10/exec/方法练习01/main.go
@@ -45,6 +45,66 @@ func (mu MethodUtils) JudgeNum(num int) {
 	}
 }
 
+/* 3、
+根据行、列、字符打印 对应行数和列数的字符，
+比如：行：3，列：2，字符*,则打印相应的效果
+
+*/
+
+func (mu *MethodUtils) print3(n int, m int, key string) {
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			fmt.Print(key)
+		}
+		fmt.Println()
+	}
+}
+
+/*
+定义小小计算器结构体(Calcuator)，
+实现加减乘除四个功能
+实现形式1：分四个方法完成: , 分别计算 + - * /
+实现形式2：用一个方法搞定, 需要接收两个数，还有一个运算符
+
+*/
+//实现形式1
+
+// 定义结构体
+type Calcuator struct {
+	Num1 float64
+	Num2 float64
+}
+
+func (calcuator *Calcuator) getSum() float64 {
+	return calcuator.Num1 + calcuator.Num2
+}
+func (calcuator *Calcuator) getStb() float64 {
+	return calcuator.Num1 - calcuator.Num2
+}
+func (calcuator *Calcuator) getSum3() float64 {
+	return calcuator.Num1 * calcuator.Num2
+}
+func (calcuator *Calcuator) getSum4() float64 {
+	return calcuator.Num1 / calcuator.Num2
+}
+
+// 实现形式2
+func (calcuator *Calcuator) getRes(operator byte) float64 {
+	res := 0.0
+	switch operator {
+	case '+':
+		res = calcuator.Num1 + calcuator.Num2
+	case '-':
+		res = calcuator.Num1 - calcuator.Num2
+	case '/':
+		res = calcuator.Num1 / calcuator.Num2
+	case '*':
+		res = calcuator.Num2 * calcuator.Num2
+	default:
+		fmt.Println("运算符输入有误...")
+	}
+	return res
+}
 func main() {
 	/*
 		1)编写结构体(MethodUtils)，编程一个方法，方法不需要参数，
@@ -62,5 +122,19 @@ func main() {
 
 	// 4 、判断一个数是奇数还是偶数
 	(&mu).JudgeNum(10)
+
+	// 5 根据行、列、字符打印 对应行数和列数的字符
+	mu.print3(7, 20, "#")
+
+	// 测试
+	var calcuator Calcuator
+	calcuator.Num1 = 1.1
+	calcuator.Num2 = 2.2
+
+	fmt.Printf("sum=%v\n", fmt.Sprintf("%.2f", calcuator.getSum()))
+	fmt.Println("sum=", fmt.Sprintf("%.2f", calcuator.getStb()))
+
+	res := calcuator.getRes('*')
+	fmt.Println("res=", fmt.Sprintf("%.2f", res))
 
 }

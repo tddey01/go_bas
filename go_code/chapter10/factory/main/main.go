f03cb56cd298f594b9a542c38bc472733586b36f
access <kevinlandun@gmail.com>
2020-02-07 15:25:27 +0800

    工厂模式
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ go_code/chapter10/factory/main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"go_bas/go_code/chapter10/factory/model"
+)
+
+func main() {
+	// 创建要给Student实例
+	var stu = model.Student{
+		Name:  "tom",
+		Score: 78.9,
+	}
+	fmt.Println(stu)
+
+	// /定student结构体是首字母小写，我们可以通过工厂模式来解决
+	var stu1 = model.NewStudent("tom",88.8)
+	fmt.Println(*stu1) //&{....}
+
+}
---
+++ go_code/chapter10/factory/model/student.go
@@ -0,0 +1,25 @@
+package model
+
+// 定义一个结构体
+type Student struct {
+	Name string
+	Score float64
+}
+
+//因为 student结构体首字母小写， 因此是只能在model使用
+//工厂模式
+
+type student1 struct {
+	Name string
+	Score float64
+}
+
+//func (s *student1)
+func NewStudent(n string,s float64) *student1{
+	  return  &student1{
+	  	Name:n,
+	  	Score:s,
+	  }
+}
+
+//如果score字段首字母小写，则，在其它包不可以直接方法，我们可以提供一个方法

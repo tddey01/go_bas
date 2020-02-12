f06a9eced818df9e87a770729c85de79ebaafa82
kn <root@localhost.com>
2020-02-12 16:30:27 +0800

    接口实现 方法

--- go_code/chapter11/interfaceDetails/main.go
+++ go_code/chapter11/interfaceDetails/main.go
@@ -21,6 +21,21 @@ type integer int
 func (i integer) Say() {
 	fmt.Println("integer Say i=", i)
 }
+
+type Binterface interface {
+	Hello()
+}
+
+type Monster struct{
+
+}
+func (m Monster) Hello(){
+	fmt.Println("Monster Hello~~")
+}
+
+func (m Monster) Say(){
+	fmt.Println("Monster Say~~")
+}
 func main() {
 	var stu Stu // 结构体变量 实现了 Say() 实现了 AInterface
 	var a AInterface = stu
@@ -29,4 +44,12 @@ func main() {
 	var i integer = 10
 	var b AInterface = i
 	b.Say() // integer Say i = 10
+
+	//Monter 实现了AInterface和BInterface
+	var monster Monster
+	var a2 AInterface = monster
+	var b2 Binterface =  monster
+	a2.Say()
+	b2.Hello()
+
 }

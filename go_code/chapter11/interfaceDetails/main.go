fce67db2ac6c030ae7b7aee3f44ea40d47e50232
kn <root@localhost.com>
2020-02-12 16:20:50 +0800

    面向对象小案例

--- go_code/chapter11/interfaceDetails/main.go
+++ go_code/chapter11/interfaceDetails/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type AInterface interface {
 	Say()
@@ -14,8 +16,17 @@ func (stu Stu) Say() {
 	fmt.Println("Stu Say()")
 }
 
+type integer int
+
+func (i integer) Say() {
+	fmt.Println("integer Say i=", i)
+}
 func main() {
 	var stu Stu // 结构体变量 实现了 Say() 实现了 AInterface
 	var a AInterface = stu
 	a.Say()
+
+	var i integer = 10
+	var b AInterface = i
+	b.Say() // integer Say i = 10
 }

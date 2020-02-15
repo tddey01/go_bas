5033a1ad0f4a7490a8269a026bdd7fdd4617ae78
kn <root@localhost.com>
2020-02-15 22:15:27 +0800

    家庭收支明细 简单程序

--- go_code/chapter12/main.go
+++ go_code/chapter12/main.go
@@ -1,13 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// 生命一个变量 保存接收用户输入的选项
 	key := ""
 	// 声明一个变量 控制是否退出for循环
 	loop := true
-
+	//定义账户的余额[]
+	balance := 10000.00
+	// 每次收支的金额
+	money := 0.00
+	//每次的收支说明
+	note := ""
+	// 定义个变量  记录是否有收支行为
+	flag := false
+	// 收支的详情使用字符串来记录
+	// 当有收支时， 只需要对details 进行拼接处理
+	details := "收支\t账户金额\t收支金额\t说    明" // 空格符
 	// 显示这个主菜单
 	for {
 		fmt.Println("--------------------------记账软件--------------------------")
@@ -21,12 +33,49 @@ func main() {
 
 		switch key {
 		case "1":
-			fmt.Println("----------------------------当前收支明细记录-----------------------------------")
+			fmt.Println("\n----------------------------当前收支明细记录-----------------------------------")
+			if flag {
+				fmt.Println(details)
+			} else {
+				fmt.Println("当前没有收支明细... 来一笔吧!")
+			}
 		case "2":
+			fmt.Println("本地收入金额")
+			fmt.Scanln(&money)
+			balance += money //修改账户余额
+			fmt.Println("本地收入说明:")
+			fmt.Scanln(&note)
+			// 将这个收入情况，拼接到details变量里面
+			//收入    11000           1000            有人发红包
+			details += fmt.Sprintf("\n收入\t%v\t%v \t  %v", balance, money, note)
+			flag = true
 		case "3":
-			fmt.Println("登记支出")
+			fmt.Println("本次支付的金额")
+			fmt.Scanln(&money)
+			//这里需要做一个必要判断
+			if money > balance {
+				fmt.Println("账户余额不足")
+				break
+			}
+			balance -= money
+			fmt.Println("本次支出说明：")
+			fmt.Scanln(&note)
+			details += fmt.Sprintf("\n支出\t%v\t%v \t  %v", balance, money, note)
+
 		case "4":
-			loop = false
+			fmt.Println("你确定要退出吗? y/n")
+			choice := ""
+			for {
+				fmt.Scanln(&choice)
+				if choice == "y" || choice == "n" {
+					break
+				}
+				fmt.Println("你的输入有误 请重新输入？ y/n")
+			}
+			if choice == "y" {
+				loop = false
+			}
+
 		default:
 			fmt.Println("请输入正确的选项")
 		}

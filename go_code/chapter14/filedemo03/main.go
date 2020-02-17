cd9ca011eb76f51cb1e62dce28adb6d380832499
access <kevinlandun@gmail.com>
2020-02-17 18:08:07 +0800

    第二种读取文件方式
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ go_code/chapter14/filedemo03/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	//使用ioutil.ReadFile 一次性将文件读取到位
+	file := "./test.txt"
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Printf("read file err=%v", err)
+	}
+	// 大读取到的内容显示到终端
+	fmt.Printf("%v\n", content) // []byte类型
+	fmt.Printf("%v\n", string(content))
+	//我们没有显式的Open文件，因此也不需要显式的Close文件
+	//因为，文件的Open和Close被封装到 ReadFile 函数内部
+}

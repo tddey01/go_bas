e6b431670f874279a867e6e638f59a2421828665
access <kevinlandun@gmail.com>
2020-02-18 11:30:11 +0800

    创建一个新文件
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ go_code/chapter14/filedemo04/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	//创建一个新文件，写入内容 5句 "hello, Gardon"
+	//1 .打开文件 test.txt
+	filePath := "./test.txt"
+	//file, err := os.OpenFile(filePath, O_WRONLY|O_CREATE, 0666)
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Printf("open file err=%v\n", err)
+		return
+	}
+	// 要及时关闭句柄，防止内存泄漏
+	defer file.Close()
+	//准备写入5句 "hello Gardon"
+	str := "hello,Gardon\n" // /n表示换行符 转义
+
+	// 写入时使用带缓存的 *Writer
+	writer := bufio.NewWriter(file)
+	for i := 0; i < 5; i++ {
+		writer.WriteString(str)
+	}
+
+	// 因为writer是自带缓存的，因此在调用WriteString方法时，其实内容先写入到缓存的,所以需要调用Flush这个方法，将缓存数据真正的写入到文件 数据落地磁盘,否则文件会丢失数据
+	writer.Flush()
+}

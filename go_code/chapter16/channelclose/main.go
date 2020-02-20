b23a6fb309fa3264e99850d81372dbd695afb59a
access <kevinlandun@gmail.com>
2020-02-20 13:36:17 +0800

    channel 遍历 案例
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_code/chapter16/channelclose/main.go
+++ go_code/chapter16/channelclose/main.go
@@ -11,6 +11,22 @@ func main() {
 	// intChan<-400
 	fmt.Println("ok")
 	// 当这个管道关闭后 可以正常读取管道里面的数据
-	n1 := <- intChan
+	n1 := <-intChan
 	fmt.Println(n1)
+
+	// 遍历管道
+	intChan2 := make(chan int, 100)
+	for i := 0; i < 100; i++ {
+		intChan2 <- i * 2 // 放入100个数据到这个管道里面
+	}
+	// 遍历 01
+	// for i := 0; i < len(intChan2); i++ {
+	// 	fmt.Println(i)
+	// }  //禁止这种遍历
+	//在遍历时，如果channel没有关闭，则会出现deadlock的错误
+	//在遍历时，如果channel已经关闭，则会正常遍历数据，遍历完后，就会退出遍历
+	close(intChan2)
+	for v := range intChan2 {
+		fmt.Println("v=", v)
+	}
 }

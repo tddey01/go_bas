791cc36def6abe93df6c98a4c7ce50e5338a84a9
access <kevinlandun@gmail.com>
2020-02-20 13:12:09 +0800

    channel  基本练习使用案例
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ go_code/chapter16/execchanneldemo01/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main(){
+	var mapChan chan map[string]string 
+	mapChan = make(chan map[string]string,10)
+	m1 := make(map[string]string,20)
+	m1 ["city1"] = "北京"
+	m1 ["city2"] = "天津"
+
+	m2 := make(map[string]string,20)
+	m2["hero1"] = "宋江"
+	m2["hero2"] = "武松"
+
+	mapChan <- m1
+	mapChan <- m2
+}
---
+++ go_code/chapter16/execchanneldemo02/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Cat struct{
+	Name string
+	Age int
+}
+
+func main(){
+	var catChan chan *Cat
+	catChan = make(chan *Cat, 10)
+	cat1 := Cat{Name:"tom",Age:18,}
+	cat2 := Cat{Name:"tom1",Age:20,}
+
+	catChan <- &cat1
+	catChan <- &cat2
+
+	// 取出
+	cat11 :=<- catChan
+	cat22 := <- catChan
+
+	fmt.Println(cat11,cat22)
+
+}

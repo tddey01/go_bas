23ee799a26f174ac8df6cd64fc04124dfc513866
kn <root@localhost.com>
2020-02-23 18:28:46 +0800

    修正

--- go_code/chatroom/client/login.go
+++ go_code/chatroom/client/login.go
@@ -5,6 +5,6 @@ import "fmt"
 // 写一个函数，完成登录
 func login(userId int, userPwd string) (err error) {
 	// 开始定协议...
-	fmt.Printf("userId=%v userPwd = %v\n", userId, userPwd)
+	fmt.Printf("userId=%v userPwd=%v\n", userId, userPwd)
 	return nil
 }
--- go_code/chatroom/client/main.go
+++ go_code/chatroom/client/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 //  定义两个变量 一个表示用户id 一个表示用户密码
-
-var userId int
-var userPwd string
+var (
+	userId  int
+	userPwd string
+)
 
 func main() {
 	// 接收用户的选择
@@ -20,7 +24,6 @@ func main() {
 		fmt.Println("\t\t\t 3 退出系统")
 		fmt.Println("\t\t\t 3 请选择(1-3)")
 
-
 		fmt.Scanf("%d\n", &key)
 
 		switch key {
@@ -32,7 +35,8 @@ func main() {
 			loop = false
 		case 3:
 			fmt.Println("退出系统")
-			loop = false
+			// loop = false
+			os.Exit(0)
 		default:
 			fmt.Println("你的输入有误 请重新输入")
 		}
@@ -40,10 +44,10 @@ func main() {
 	// 根据用户输入 显示新的提示信息
 	if key == 1 {
 		// 说明用户要登录
-		fmt.Println("请输入用户id号")
+		fmt.Println("请输入用户id")
 		fmt.Scanf("%d\n", &userId)
 		fmt.Println("请输入用户密码")
-		fmt.Scanf("%d\n", &userPwd)
+		fmt.Scanf("%s\n", &userPwd)
 		// 把登录的函数 写到另一个文件内 比如写在login文件
 		err := login(userId, userPwd)
 		if err != nil {

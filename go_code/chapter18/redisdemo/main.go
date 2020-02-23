4a2b1a8542a5557780139876b678d87d9ed34443
kn <root@localhost.com>
2020-02-23 13:23:11 +0800

    redis初始操作

---
+++ go_code/chapter18/redisdemo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gomodule/redigo/redis" // 引入redis
+)
+
+func main() {
+	// 通过go 向redis  写入数据和读取数据
+	// 1 链接redis
+	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
+	if err != nil {
+		fmt.Println("redis.Dial err=", err)
+		return
+	}
+	// 通过go向redis写入数据string[key-value]
+	_, err = conn.Do("set", "name", "tomjerry")
+	if err != nil {
+		fmt.Println("set err=", err)
+		return
+	}
+	// 通过go 向redis读取数据 string [key-value]
+	r, err = conn.Do("Get", "name")
+	if err != nil {
+		fmt.Println("Get err=", err)
+		return
+	}
+	// 因为返回r‘是interface{}
+	// 因为name 对应的执行时string，因此我们需要转换
+	nameString := r.S
+	fmt.Println("conn succ...")
+}

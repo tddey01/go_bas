3f15e7ea339acc05c9609b6eb3f08ccaec4332ba
access <kevinlandun@gmail.com>
2020-03-10 16:53:34 +0800

    多路复用器
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_web/day01/web01/main.go
+++ go_web/day01/web01/main.go
@@ -7,12 +7,17 @@ import (
 
 // 创建处理器函数
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello World!", r.URL.Path)
+	//fmt.Fprintln(w, "hello World!", r.URL.Path)
+	fmt.Fprintln(w, "通过自己创建多路复用器请求", r.URL.Path)
 }
 
 func main() {
+	//创建多路复用器
+	mux := http.NewServeMux()
 	// 处理请求
-	http.HandleFunc("/", handler)
+	//http.HandleFunc("/", handler)
+	mux.HandleFunc("/", handler)
 	//创建路由
-	http.ListenAndServe(":8080", nil)
+	//http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 }

fbb3e1f0a4c292bdca2b7755fc2be2915bbdc7bf
access <kevinlandun@gmail.com>
2020-03-10 16:20:24 +0800

    创建服务器详细配置
    Signed-off-by: access <kevinlandun@gmail.com>

--- go_web/webapp/web02/main.go
+++ go_web/webapp/web02/main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 // 结构体
 type MyHandler struct{}
 
 func (m *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "我们自己创建的处理器请求！")
+	//fmt.Fprintln(w, "我们自己创建的处理器请求！")
+	fmt.Fprintln(w, "通过详细配置服务器的信息处理器请求！")
 }
 func main() {
 	//
 	myHandler := MyHandler{}
 	// 处理请求
-	http.Handle("/myHandler", &myHandler)
+	// http.Handle("/myHandler", &myHandler)
+	// 创建server结构 详细配置里面字段
+	server := http.Server{
+		Addr:        ":8080",
+		Handler:     &myHandler,
+		ReadTimeout: time.Second * 2,
+	}
 	// 路由分发
-	http.ListenAndServe(":8080", nil)
+	//http.ListenAndServe(":8080", nil)
+	server.ListenAndServe()
 }

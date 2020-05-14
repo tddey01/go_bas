3d19d8dc2e683f4b3ebcce3b9f6d5762d85e5b5d
kn <kn@gmail.com>
2020-05-14 19:40:27 +0800

    增加

--- iris_web/002/002/main.go
+++ iris_web/002/002/main.go
@@ -1,35 +1,54 @@
 package main
 
 import (
-	"github.com/go_bas/iris_web/002/002/config"
+	"fmt"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
-	"github.com/kataras/iris/mvc"
-	"github.com/go_bas/iris_web/002/002/service"
-	"github.com/go_bas/iris_web/002/002/controller"
-	"github.com/go_bas/iris_web/002/002/datasource"
 	"github.com/kataras/iris/sessions"
+	"github.com/tddey01/go_bas/iris_web/002/002/config"
 	"time"
 )
 
 // newApp()  构建
 func newApp() *iris.Application {
 	app := iris.New()
+
 	//设置日志级别  开发阶段为debug
 	app.Logger().SetLevel("debug")
 
 	// 注册静态资源
-	app.StaticWeb()
+	app.HandleDir("/static", "./static")
+	app.HandleDir("/manage/static", "./static")
+
+	//注册视图文件
+	app.RegisterView(iris.HTML("/static", ".html"))
+	app.Get("/", func(context context.Context) {
+		path := context.Path()
+		app.Logger().Info(path)
+		context.View("index.html")
+	})
+	return app
+}
+
+// mvcHandler  * MVC 架构模式处理
+
+func mvcHandler(app *iris.Application) {
+	//启用session
+	sessManager := sessions.New(sessions.Config{
+		Cookie:  "sessioncookie",
+		Expires: 24 * time.Hour,
+	})
+	//engine := datasource.
 }
 
 func main() {
 	app := newApp()
 
 	// 应用设置
-	configuration(app)
+	//configuration(app)
 
 	// 路由设置
-	mvcHandle(app)
+	//mvcHandle(app)
 
 	config := config.InitConfig()
 }
